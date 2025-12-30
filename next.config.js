7ab3c91373cc709394933a3aef6749a648a2b242
blin1124 <blingamells@gmail.com>
2025-12-30 10:18:45 -0600

    Remove unnecessary comments from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
+ /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // IMPORTANT:
-  // Do NOT use `output: "export"` for this app (it breaks auth + API routes).
-  // Vercel deploy should be normal Next.js server build.
 };
 
 module.exports = nextConfig;
+
