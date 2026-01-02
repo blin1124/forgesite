23be6df8240e248b29799eaff0d4fe17732f636b
blin1124 <blingamells@gmail.com>
2026-01-02 17:27:19 -0600

    Rename SuccessClient to Client in ProSuccessPage

--- app/pro/success/page.tsx
+++ app/pro/success/page.tsx
@@ -1,9 +1,10 @@
-import SuccessClient from "./Client";
+import Client from "./Client";
 
 export const dynamic = "force-dynamic";
 
 export default function ProSuccessPage() {
-  return <SuccessClient />;
+  return <Client />;
 }
+
 
 
