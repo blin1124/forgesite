4c3198189e0ae296eb4c2b4ba9b722f5330db309
blin1124 <blingamells@gmail.com>
2026-01-05 08:41:26 -0600

    Rename import from Client to BuilderClient

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,10 +1,11 @@
-import BuilderClient from "./Client";
+import BuilderClient from "./BuilderClient";
 
 export const dynamic = "force-dynamic";
 
 export default function BuilderPage() {
   return <BuilderClient />;
 }
+
 
 
 
