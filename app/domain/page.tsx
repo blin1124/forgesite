10dd40d26856735db59b377e6b611fc62fde9dcd
blin1124 <blingamells@gmail.com>
2026-01-06 22:42:21 -0600

    Rename DomainClient to Client in DomainPage

--- app/domain/page.tsx
+++ app/domain/page.tsx
@@ -1,10 +1,11 @@
-import DomainClient from "./Client";
+import Client from "./Client";
 
 export const dynamic = "force-dynamic";
 
 export default function DomainPage() {
-  return <DomainClient />;
+  return <Client />;
 }
+
 
 
 
